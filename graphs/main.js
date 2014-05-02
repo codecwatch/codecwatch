ac573057298ec29672507c9ea23cc0074b030ab7
La Spada Luca <luca.laspada@epfl.ch>
2014-05-02 19:02:59 +0200

    Add calendar feature

--- graphs/main.js
+++ graphs/main.js
@@ -8,8 +8,53 @@ function mapEncoGitToLink(encoder, gitrev) {
 	return "<a href=" + mapEncoder[encoder]+gitrev + ">Git revison</a>"; 
 }
 
-
 $(function() {
+
+	var myControl=  {
+		create: function(tp_inst, obj, unit, val, min, max, step){
+			$('<input class="ui-timepicker-input" value="'+val+'" style="width:50%">')
+				.appendTo(obj)
+				.spinner({
+					min: min,
+					max: max,
+					step: step,
+					change: function(e,ui){ // key events
+							// don't call if api was used and not key press
+							if(e.originalEvent !== undefined)
+								tp_inst._onTimeChange();
+							tp_inst._onSelectHandler();
+						},
+					spin: function(e,ui){ // spin events
+							tp_inst.control.value(tp_inst, obj, unit, ui.value);
+							tp_inst._onTimeChange();
+							tp_inst._onSelectHandler();
+						}
+				});
+			return obj;
+		},
+		options: function(tp_inst, obj, unit, opts, val){
+			if(typeof(opts) == 'string' && val !== undefined)
+				return obj.find('.ui-timepicker-input').spinner(opts, val);
+			return obj.find('.ui-timepicker-input').spinner(opts);
+		},
+		value: function(tp_inst, obj, unit, val){
+			if(val !== undefined)
+				return obj.find('.ui-timepicker-input').spinner('value', val);
+			return obj.find('.ui-timepicker-input').spinner('value');
+	}};
+	
+
+	$('#dateInputFrom').datetimepicker({
+		controlType: myControl,
+		timeFormat: 'HH:mm:ss'
+	});
+
+	$('#dateInputTo').datetimepicker({
+		controlType: myControl,
+		timeFormat: 'HH:mm:ss'
+	});
+
+
 	var input = [
 	{
 		"encoder": "x264",
